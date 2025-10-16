d7472a34f3e75008a7c532d0415dcd7f4ea768a9
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-10-16 17:04:42 -0600

    infraestructure layer

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,27 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) {
+    return Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+          : 'https://i.stack.imgur.com/GNhxO.png',
+      genreIds: List<String>.from(movieDB.genreIds),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+          : 'https://i.stack.imgur.com/GNhxO.png',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount,
+    );
+  }
+}
