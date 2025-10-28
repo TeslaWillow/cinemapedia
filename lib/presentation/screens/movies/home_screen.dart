3fb22c4e917017199022152fc92f0138ce7d6f3f
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-10-28 17:34:42 -0600

    custom button navigation bar.

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -13,6 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 }
